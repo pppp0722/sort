acf37b36b570e2aae984e33bc0b5e54026f64ad1
pppp0722 <poj0722@naver.com>
2022-05-24 03:38:12 +0900

    [add] 퀵 정렬(Quick Sort)

--- src/Sort.java
+++ src/Sort.java
@@ -41,6 +41,20 @@ public class Sort {
             System.out.print(num + " ");
         }
         System.out.println("\n");
+
+        int[] arr6 = new int[]{10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        sortByQuickSort(arr6);
+        System.out.println("-Quick Sort-");
+        for (int num : arr6) {
+            System.out.print(num + " ");
+        }
+        System.out.println("\n");
+    }
+
+    public static void swap(int[] arr, int idx1, int idx2) {
+        int tmp = arr[idx1];
+        arr[idx1] = arr[idx2];
+        arr[idx2] = tmp;
     }
 
     // 선택 정렬(Selection Sort) -> worst & average & best = O(n^2)
@@ -52,9 +66,7 @@ public class Sort {
                     min = j;
                 }
             }
-            int tmp = arr[i];
-            arr[i] = arr[min];
-            arr[min] = tmp;
+            swap(arr, i, min);
         }
     }
 
@@ -63,9 +75,7 @@ public class Sort {
         for (int i = 1; i < arr.length; i++) {
             for (int j = i - 1; j >= 0; j--) {
                 if (arr[j] > arr[j + 1]) {
-                    int tmp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = tmp;
+                    swap(arr, j, j + 1);
                 }
             }
         }
@@ -76,9 +86,7 @@ public class Sort {
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    int tmp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = tmp;
+                    swap(arr, j, j + 1);
                 }
             }
         }
@@ -86,38 +94,38 @@ public class Sort {
 
     // 합병(병합) 정렬(Merge Sort) -> worst & average & best = O(nlog n)
     public static void sortByMergeSort(int[] arr) {
-        int[] tmp = new int[arr.length];
-        mergeSort(arr, tmp, 0, arr.length - 1);
+        int[] tmpArr = new int[arr.length];
+        mergeSort(arr, tmpArr, 0, arr.length - 1);
     }
 
-    public static void mergeSort(int[] arr, int[] tmp, int l, int r) {
-        if (l < r) {
-            int m = l + (r - l) / 2;
-            mergeSort(arr, tmp, l, m);
-            mergeSort(arr, tmp, m + 1, r);
-            merge(arr, tmp, l, m, r);
+    public static void mergeSort(int[] arr, int[] tmpArr, int left, int right) {
+        if (left < right) {
+            int m = left + (right - left) / 2;
+            mergeSort(arr, tmpArr, left, m);
+            mergeSort(arr, tmpArr, m + 1, right);
+            merge(arr, tmpArr, left, m, right);
         }
     }
 
-    public static void merge(int[] arr, int[] tmp, int l, int m, int r) {
-        for (int i = l; i <= r; i++) {
-            tmp[i] = arr[i];
+    public static void merge(int[] arr, int[] tmpArr, int left, int mid, int right) {
+        for (int i = left; i <= right; i++) {
+            tmpArr[i] = arr[i];
         }
-        int part1 = l;
-        int part2 = m + 1;
-        int index = l;
-        while (part1 <= m && part2 <= r) {
-            if (tmp[part1] <= tmp[part2]) {
-                arr[index] = tmp[part1];
+        int part1 = left;
+        int part2 = mid + 1;
+        int index = left;
+        while (part1 <= mid && part2 <= right) {
+            if (tmpArr[part1] <= tmpArr[part2]) {
+                arr[index] = tmpArr[part1];
                 part1++;
             } else {
-                arr[index] = tmp[part2];
+                arr[index] = tmpArr[part2];
                 part2++;
             }
             index++;
         }
-        for (int i = 0; i <= m - part1; i++) {
-            arr[index + i] = tmp[part1 + i];
+        for (int i = 0; i <= mid - part1; i++) {
+            arr[index + i] = tmpArr[part1 + i];
         }
     }
 
@@ -126,17 +134,10 @@ public class Sort {
         for (int i = arr.length / 2 - 1; i < arr.length; i++) {
             heapify(arr, i, arr.length - 1);
         }
-
         for (int i = arr.length - 1; i >= 0; i--) {
             swap(arr, 0, i);
             heapify(arr, 0, i - 1);
         }
-    }
-
-    public static void swap(int[] arr, int idx1, int idx2) {
-        int tmp = arr[idx1];
-        arr[idx1] = arr[idx2];
-        arr[idx2] = tmp;
     }
 
     public static void heapify(int[] arr, int parentIdx, int lastIdx) {
@@ -152,11 +153,9 @@ public class Sort {
             if (arr[leftChildIdx] > arr[largestIdx]) {
                 largestIdx = leftChildIdx;
             }
-
             if (rightChildIdx <= lastIdx && arr[rightChildIdx] > arr[largestIdx]) {
                 largestIdx = rightChildIdx;
             }
-
             if (largestIdx != parentIdx) {
                 swap(arr, parentIdx, largestIdx);
                 parentIdx = largestIdx;
@@ -164,5 +163,38 @@ public class Sort {
                 break;
             }
         }
+    }
+
+    // 퀵 정렬(Quick Sort) -> average & best = O(nlog n), worst = O(n^2)
+    public static void sortByQuickSort(int[] arr) {
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    public static void quickSort(int[] arr, int left, int right) {
+        int part = partition(arr, left, right);
+        if (left < part - 1) {
+            quickSort(arr, left, part - 1);
+        }
+        if (part < right) {
+            quickSort(arr, part, right);
+        }
+    }
+
+    public static int partition(int[] arr, int left, int right) {
+        int pivot = arr[(left + right) / 2];
+        while (left <= right) {
+            while (arr[left] < pivot) {
+                left++;
+            }
+            while (arr[right] > pivot) {
+                right--;
+            }
+            if (left <= right) {
+                swap(arr, left, right);
+                left++;
+                right--;
+            }
+        }
+        return left;
     }
 }
